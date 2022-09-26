6a46f70e05b703fe01e4ce2754309a6e45b1e8c1
Tran Binh Co <tranbinhco99@gmail.com>
2022-09-26 10:52:53 +0700

    add tailwind scss to project

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from './routes';
 import { DefaultLayout } from './components/Layouts';
 import GlobalStyles from './components/GlobalStyles';
-import { MessengerChat } from 'react-messenger-chat-plugin';
 function App(): JSX.Element {
     return (
         <GlobalStyles>
--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,5 +1,36 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Home.module.scss';
 function Home() {
-    return <div>Home</div>
+    const [users, setUserList] = useState([null]);
+    useEffect(() => {
+        // const url = 'http://localhost:5000/api/v1/users';
+        // axios
+        //     .get(url)
+        //     .then((response) => {
+        //         console.log('response: ', response.data);
+        //     })
+        //     .catch((err) => {
+        //         console.log('err', err);
+        //     });
+        fetchApi();
+    }, []);
+
+    const fetchApi = async () => {
+        const url = 'http://localhost:5000/api/v1/users';
+        const response = await axios.get(url).then((response) => {
+            console.log(response.data);
+            setUserList(response.data);
+        });
+        return response;
+    };
+    console.log('users: ', users);
+
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </div>
+    );
 }
 
 export default Home;
