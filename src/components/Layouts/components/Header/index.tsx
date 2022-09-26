d63af929c1cf7b2d78ac621da02f80bea1961988
Tran Binh Co <tranbinhco99@gmail.com>
2022-09-26 15:45:35 +0700

    create header and subheader component by tailwind

--- src/components/Layouts/components/Header/index.tsx
+++ src/components/Layouts/components/Header/index.tsx
@@ -1,25 +1,185 @@
-import classNames from 'classnames/bind';
+import { Fragment } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon, UserIcon } from '@heroicons/react/24/outline';
 import styles from './Header.module.scss';
+import { Link } from 'react-router-dom';
+const navigation = [
+    { name: 'Trang chủ', href: '/', current: true },
+    { name: 'Sự kiện', href: '/news', current: false },
+    { name: 'Tuyển dụng', href: '/recruit', current: false },
+    { name: 'Liên hệ', href: '/contact', current: false },
+];
 
-const cx = classNames.bind(styles);
+function classNames(...classes: any) {
+    return classes.filter(Boolean).join(' ');
+}
 
 function Header() {
     return (
-        <header className={cx('wrapper')}>
-            <div className={cx('outer')}>
-                <div className={cx('container')}>
-                    <div className={cx('inner')}>
-                        <div className={cx('logo')}>
-                            <img
-                                src={require('../../../../resources/logo-mamnonphuongthy.png')}
-                                alt="logo-mam-non-phuong-thy"
-                            />
-                            <h1 className={cx('logo-title')}>MẦM NON PHƯƠNG THY</h1>
+        <Disclosure as="nav" className="bg-gray-800 ">
+            {({ open }) => (
+                <>
+                    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                        <div className="relative flex h-16 items-center justify-between">
+                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                {/* Mobile menu button*/}
+                                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                    <span className="sr-only">Mở trang chính</span>
+                                    {open ? (
+                                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                                    ) : (
+                                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                                    )}
+                                </Disclosure.Button>
+                            </div>
+                            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                                <div className="flex flex-shrink-0 items-center">
+                                    <img
+                                        className="block h-8 w-auto lg:hidden"
+                                        src="https://lh3.googleusercontent.com/pw/AL9nZEUi-wNa3cQBL1tRr3-PEfSbIlpSmU-lZxPNeVf8UpCwcGto7bSUtky1S0bU4buuxyKtJ4_GTnCMUUDT2dyc3V2-DmmDNHGMHGkWO0MrF5tm46eFJK5QgTtZLmv51cSrJ_OHiRJs163ntFuXcwka_cGs=w393-h362-no?authuser=0"
+                                        alt="Mam Non Phuong Thy"
+                                    />
+                                    <img
+                                        className="hidden h-8 w-auto lg:block"
+                                        src="https://lh3.googleusercontent.com/pw/AL9nZEUi-wNa3cQBL1tRr3-PEfSbIlpSmU-lZxPNeVf8UpCwcGto7bSUtky1S0bU4buuxyKtJ4_GTnCMUUDT2dyc3V2-DmmDNHGMHGkWO0MrF5tm46eFJK5QgTtZLmv51cSrJ_OHiRJs163ntFuXcwka_cGs=w393-h362-no?authuser=0"
+                                        alt="Mam Non Phuong Thy"
+                                    />
+                                </div>
+                                <div className="hidden sm:ml-6 sm:block">
+                                    <div className="flex space-x-4">
+                                        {navigation.map((item) => (
+                                            <Link
+                                                onClick={() => {
+                                                    if (item.current) {
+                                                        return;
+                                                    } else {
+                                                        navigation.forEach((value) => {
+                                                            value.current = false;
+                                                        });
+                                                        item.current = true;
+                                                    }
+                                                }}
+                                                to={item.href}
+                                                key={item.name}
+                                                className={classNames(
+                                                    item.current
+                                                        ? 'bg-gray-900 text-white'
+                                                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                    'px-3 py-2 rounded-md text-sm font-medium',
+                                                )}
+                                                aria-current={item.current}
+                                            >
+                                                {item.name}
+                                            </Link>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                                {/* <button
+                                    type="button"
+                                    className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                >
+                                    <span className="sr-only">View notifications</span>
+                                    <BellIcon className="h-6 w-6" aria-hidden="true" />
+                                </button> */}
+
+                                {/* Profile dropdown */}
+                                <Menu as="div" className="relative ml-3">
+                                    <div>
+                                        <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                            <span className="sr-only">Open user menu</span>
+                                            <UserIcon className="block h-6 w-6 text-white" aria-hidden="true" />
+                                        </Menu.Button>
+                                    </div>
+                                    <Transition
+                                        as={Fragment}
+                                        enter="transition ease-out duration-100"
+                                        enterFrom="transform opacity-0 scale-95"
+                                        enterTo="transform opacity-100 scale-100"
+                                        leave="transition ease-in duration-75"
+                                        leaveFrom="transform opacity-100 scale-100"
+                                        leaveTo="transform opacity-0 scale-95"
+                                    >
+                                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <a
+                                                        href="#"
+                                                        className={classNames(
+                                                            active ? 'bg-gray-100' : '',
+                                                            'block px-4 py-2 text-sm text-gray-700',
+                                                        )}
+                                                    >
+                                                        Trang cá nhân
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <a
+                                                        href="#"
+                                                        className={classNames(
+                                                            active ? 'bg-gray-100' : '',
+                                                            'block px-4 py-2 text-sm text-gray-700',
+                                                        )}
+                                                    >
+                                                        Cài đặt
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <a
+                                                        href="#"
+                                                        className={classNames(
+                                                            active ? 'bg-gray-100' : '',
+                                                            'block px-4 py-2 text-sm text-gray-700',
+                                                        )}
+                                                    >
+                                                        Đăng xuất
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                        </Menu.Items>
+                                    </Transition>
+                                </Menu>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </header>
+
+                    <Disclosure.Panel className="sm:hidden">
+                        <div className="space-y-1 px-2 pt-2 pb-3">
+                            {navigation.map((item) => (
+                                <Link
+                                    onClick={() => {
+                                        if (item.current) {
+                                            return;
+                                        } else {
+                                            navigation.forEach((value) => {
+                                                value.current = false;
+                                            });
+                                            item.current = true;
+                                        }
+                                    }}
+                                    key={item.name}
+                                    to={item.href}
+                                    className={classNames(
+                                        item.current
+                                            ? 'bg-gray-900 text-white'
+                                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                        'block px-3 py-2 rounded-md text-base font-medium',
+                                    )}
+                                    aria-current={item.current ? 'page' : undefined}
+                                >
+                                    {item.name}
+                                </Link>
+                            ))}
+                        </div>
+                    </Disclosure.Panel>
+                </>
+            )}
+        </Disclosure>
     );
 }
 
--- src/components/Layouts/components/SubHeader/index.tsx
+++ src/components/Layouts/components/SubHeader/index.tsx
@@ -21,24 +21,43 @@ function SubHeader() {
                                 'col-xl-10',
                             )}
                         >
-                            <a
-                                href={
-                                    'https://www.google.com/maps/place/M%E1%BA%A7m+Non+T%C6%B0+Th%E1%BB%A5c+Ph%C6%B0%C6%A1ng+Thy/@10.1190909,106.629646,17z/data=!4m5!3m4!1s0x319ffdfcd72b994d:0xf5477626fd9abaf!8m2!3d10.1190909!4d106.6318347'
-                                }
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <FontAwesomeIcon className={cx('icon')} icon={faMap} />
-                                Vị trí trên bản đồ
-                            </a>
-                            <a href={'mailto:mamnonphuongthy@gmail.com'}>
-                                <FontAwesomeIcon className={cx('icon')} icon={faEnvelope} />
-                                mamnonphuongthy@gmail.com
-                            </a>
-                            <a href={'tel:+84963940669'}>
-                                <FontAwesomeIcon className={cx('icon')} icon={faPhone} />
-                                SĐT: 0963-940-669
-                            </a>
+                            <div className={cx('icon-responsive-desktop')}>
+                                <a
+                                    href={
+                                        'https://www.google.com/maps/place/M%E1%BA%A7m+Non+T%C6%B0+Th%E1%BB%A5c+Ph%C6%B0%C6%A1ng+Thy/@10.1190909,106.629646,17z/data=!4m5!3m4!1s0x319ffdfcd72b994d:0xf5477626fd9abaf!8m2!3d10.1190909!4d106.6318347'
+                                    }
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <FontAwesomeIcon className={cx('icon')} icon={faMap} />
+                                    Vị trí trên bản đồ
+                                </a>
+                                <a href={'mailto:mamnonphuongthy@gmail.com'}>
+                                    <FontAwesomeIcon className={cx('icon')} icon={faEnvelope} />
+                                    mamnonphuongthy@gmail.com
+                                </a>
+                                <a href={'tel:+84963940669'}>
+                                    <FontAwesomeIcon className={cx('icon')} icon={faPhone} />
+                                    SĐT: 0963-940-669
+                                </a>
+                            </div>
+                            <div className={cx('icon-responsive-mobile')}>
+                                <a
+                                    href={
+                                        'https://www.google.com/maps/place/M%E1%BA%A7m+Non+T%C6%B0+Th%E1%BB%A5c+Ph%C6%B0%C6%A1ng+Thy/@10.1190909,106.629646,17z/data=!4m5!3m4!1s0x319ffdfcd72b994d:0xf5477626fd9abaf!8m2!3d10.1190909!4d106.6318347'
+                                    }
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <FontAwesomeIcon className={cx('icon')} icon={faMap} />
+                                </a>
+                                <a href={'mailto:mamnonphuongthy@gmail.com'}>
+                                    <FontAwesomeIcon className={cx('icon')} icon={faEnvelope} />
+                                </a>
+                                <a href={'tel:+84963940669'}>
+                                    <FontAwesomeIcon className={cx('icon')} icon={faPhone} />
+                                </a>
+                            </div>
                         </div>
                         <div
                             className={cx('social-contact', 'col-xs-3', 'col-sm-3', 'col-md-2', 'col-lg-2', 'col-xl-2')}
